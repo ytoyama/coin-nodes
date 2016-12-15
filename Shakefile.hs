db24f8cc1c9d735057c016af6e6806e11ada233d
Yota Toyama <raviqqe@gmail.com>
2016-12-15 10:51:50 +0900

    Add ytoyama target

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Development.Shake
 import Development.Shake.Command
 import Development.Shake.FilePath
@@ -11,7 +12,10 @@ hostsFile = "hosts"
 main :: IO ()
 main = shakeArgs shakeOptions $ do
   "ping" ~> cmd "ansible all -i" hostsFile "-m ping"
-  "setup" ~> do
-    cmd "ansible-playbook --ask-become-pass -i" hostsFile "main.yml"
+
+  forM_ ["common", "ytoyama"] $ \target -> do
+    target ~> do
+      cmd "ansible-playbook --ask-become-pass -i" hostsFile (target ++ ".yml")
+
   "clean" ~> do
     cmd Shell "rm -f *.retry"
