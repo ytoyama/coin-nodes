5c1d7cdd8a71d672c06f0bfd35007f87ab796821
Yota Toyama <raviqqe@gmail.com>
2016-12-15 09:21:59 +0900

    Distribute hosts file

---
+++ Shakefile.hs
@@ -0,0 +1,17 @@
+import Development.Shake
+import Development.Shake.Command
+import Development.Shake.FilePath
+import Development.Shake.Util
+
+
+
+hostsFile = "hosts"
+
+
+main :: IO ()
+main = shakeArgs shakeOptions $ do
+  "ping" ~> cmd "ansible all -i" hostsFile "-m ping"
+  "setup" ~> do
+    cmd "ansible-playbook --ask-become-pass -i" hostsFile "main.yml"
+  "clean" ~> do
+    cmd Shell "rm -f *.retry"
